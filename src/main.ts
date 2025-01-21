7b2160b9e28e73edb9293e7f64756394fce9a832
Jeferson santos <jefersonrouue@gmail.com>
2025-01-21 17:23:20 -0300

    feat : added prisma cliet settup, encriptation and crud functtions in service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client';
+import { Prisma } from '@prisma/client';
+
 
 async function bootstrap() {
+
+  //instanciar o prisma
+try {
+  
+  //instanciar prisma 
+  let prisma = new PrismaClient()
+
+  await prisma.$connect();
+
+  console.log('Conexão com o banco de dados estabelecida com sucesso!')
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
-  console.log('Servidor rodando na porta http://localhost:3333')
+  await app.listen(3333);
+
+} catch (error) {
+  console.error('Erro ao conectar ao banco de dados:', error);
+}
+ 
+
 }
 bootstrap();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    //constructor para injetar dependencia
+    constructor(private readonly prisma: PrismaService) { }
+
+    //function para criar um usuario
+    async createUser(data: Prisma.UserCreateInput) {
+        //hashiando a senha
+        const hashPassword = await bcrypt.hash(data.password, 10)
+
+        //criando copia do data e adicionando senha hasheada ao objeto data
+        return this.prisma.user.create({
+            data: {...data, password : hashPassword
+               
+            }
+        });
+    }
+
+    //function para pegar usuario
+    async user(
+        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+      ): Promise<User | null> {
+        return this.prisma.user.findUnique({
+          where: userWhereUniqueInput,
+        });
+      }
+
+    //function para update user
+    async updateUser(params: {
+        where: Prisma.UserWhereUniqueInput;
+        data: Prisma.UserUpdateInput;
+    }): Promise<User> {
+        const { where, data } = params;
+        return this.prisma.user.update({
+            data,
+            where,
+        });
+    }
+
+    //function para deletar user
+    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.delete({
+            where,
+        });
+    }
+
+
+}
